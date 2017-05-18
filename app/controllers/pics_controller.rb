1f3512d3236fcb3a4e422a5ed6ced531f211e6e2
HEY <you@example.com>
2017-05-18 14:38:50 +0530

    Create new pics actions and view files

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,2 +1,30 @@
 class PicsController < ApplicationController
+
+
+def index
+end
+
+def new
+	@pic=Pic.new
+end
+
+def create
+	@pic = Pic.new(pics_params)
+end
+
+def edit
+end
+
+def update
+end
+
+def destroy
+end
+
+ private
+
+  def pics_params
+  	params.require(:pic).permit(:title , :description)
+  end	
+
 end
